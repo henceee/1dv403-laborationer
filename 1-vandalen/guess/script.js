6d44a1e680ab257dee0ee628063b31d43ebe54ec
OpenShift guest <hg222dv@student.lnu.se>
2013-11-20 10:12:12 -0500

    guess

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -1,17 +1,64 @@
 "use strict";
+ 
+var count=0;
+
+
+
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor( Math.random() * (100-1)+1) + 1; // Detta tal behöver bytas ut mot ett slumpat tal.
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
+	    
+	    
+	    
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
+	    
+	    
+	    if(number <0 || number >100){
+	        
+	        count++;
+	        
+	        return [false, "Talet är utanför intervallet 0 - 100"];
+	        
+	    }
+	    
+	    
+	    else if (number !== secret && number < secret){
+	      
+	    count++;
+	  
+        return [false, "Det hemliga talet är högre!"];
+	   
+	        
+	    }
+	    
+	   else if (number !== secret && number > secret){
+	      
+	    count++;
+	  
+        return [false, "Det hemliga talet är lägre!"];
+	   
+	        
+	    }
+	    
+	   else if (number == secret){
+	    
+	    count++;
+	  
+        return [true, "Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+count+" gissningar för att hitta det."];
+	   
+	        
+	    }
+	    
+		
 		// Plats för förändring.
 
-
+        
+       
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
 		// [false, "Det hemliga talet är högre!"]
@@ -20,7 +67,8 @@ window.onload = function(){
 	};
 	
 	// ------------------------------------------------------------------------------
-
+    
+    
 
 
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
@@ -32,7 +80,7 @@ window.onload = function(){
 	submit.addEventListener("click", function(e){
 		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-		var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
+    	var	answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
 		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
 
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
