cf8ccc413a1907d0e6039e9ece4a0b79c3077d57
henceee <hg222dv@student.lnu.se>
2013-12-12 17:12:07 +0100

    klar med labbymezzages iaf......................

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//MessageBoard ett objekt, innehållande messages arrayen som innehåller alla meddelanden; init som körs när sidan laddas, renderMessages som renderar alla meddelande (tar bort, loopar igenom,
+// anropar renderMessage (OBS singular!).
+
 var MessageBoard = {
 
     messages: [],
@@ -8,34 +11,25 @@ var MessageBoard = {
         
         var p = document.querySelector("#value");
         var msgstring = document.querySelector("#msg");
+        var write = document.querySelector("#write");
+
+        //Om man tycker man trycker enter blir det som att man klicka på "skriv" (write.onclick)
 
         msgstring.onkeyup = function (e) {
-
-            //alert("hej");
-            var code = e.keyCode;
-
-            if ((code == 13) && event.shiftKey) {
-
-                msgstring.value.getHTMLText();
-
-            }
-
-            if (code == 13) {
-
-                //MessageBoard.init.write.onclick();
-
-                //koppla till onclick eventet på nåt sätt eller skriva ett helt nytt lika dant event? -.- Bryter ju mot DRY
-            }
+                        
            
+            if (!e.shiftKey && e.which == 13) {
+                write.click();
+                return false;
+            }
+
         }
               
 
-        var write = document.querySelector("#write");
+        // Lägger in en p-tag i div-en med id count. Skriver ut antal medd.
 
         var counter = document.querySelector("#count");
-
-        counter.innerHTML = "";
-
+                
         var countertext = document.createElement("p");
                                 
         countertext.innerHTML = ("Antal meddelande: " + MessageBoard.messages.length);
@@ -43,7 +37,7 @@ var MessageBoard = {
         counter.appendChild(countertext);
 
         
-
+        //Om man klickar på "skriv" knappen (med id write) pushas meddelandet in i messages arrayen.
 
         write.onclick = function () {
 
@@ -51,19 +45,13 @@ var MessageBoard = {
 
             var mess = new Message(input, new Date());
             
-            
             MessageBoard.messages.push(mess);
 
-            //console.log(MessageBoard.messages);
-            //console.log(countertext.innerHTML);
             countertext.innerHTML = "";
-            //console.log(countertext.innerHTML);
-            
-
+                
             var counter = document.querySelector("#count");
             counter.innerHTML = ("Antal meddelande: " + MessageBoard.messages.length);
-            //counter.appendChild(countertext);
-
+           
             MessageBoard.renderMessages();
             
 
@@ -92,15 +80,29 @@ var MessageBoard = {
 
     renderMessage: function (MessageID) {
        
-        
+        //Lägger in en p-tag med meddelandet.
+
         var text = document.createElement("p");
                 
         text.innerHTML = MessageBoard.messages[MessageID].getHTMLText();
 
         var present = document.querySelector("#present");
 
-        present.appendChild(text);
+        //Lägger in en p-tag med datumstämplen.
 
+        var dateText = document.createElement("p");
+        
+        var mezzdate = MessageBoard.messages[MessageID].getDate();
+
+        var mezzdateinner = mezzdate.getUTCHours() + ":" + mezzdate.getUTCMinutes() +":" + mezzdate.getUTCSeconds();
+
+        dateText.innerHTML = mezzdateinner;
+        
+        present.appendChild(text);
+        present.appendChild(dateText);
+
+
+        //skapar en radera-knapp, med MessageID som id, remove som klassnamn.
 
         var removebutton = document.createElement("button");
 
@@ -110,53 +112,74 @@ var MessageBoard = {
         
         present.appendChild(removebutton);
 
+        //lägger in alla med klassnamnet remove i en variabel, loopar igenom dem som en array och lägger på en onclick på dem.
 
         var remelements = document.getElementsByClassName("remove");
         
 
         for (var i = 0; i < remelements.length; i += 1) {
             remelements[i].onclick = function () {
-                //console.log(this.getAttribute("id"));
-                var mid = this.getAttribute("id");
-                MessageBoard.messages.splice(mid, 1);
-                MessageBoard.renderMessages();
 
-                var counter = document.querySelector("#count");
+                var result = confirm("Vill du verkligen radera meddelandet?");
 
-                counter.innerHTML = "";
+                //Om användaren klickar på "OK" görs en splice på Messages-arrayen. Countern uppdateras.
 
-                var countertext = document.createElement("p");
+                if (result == true) {
+
+                    
+                    var mid = this.getAttribute("id");
+                    MessageBoard.messages.splice(mid, 1);
+                    MessageBoard.renderMessages();
+
+                    var counter = document.querySelector("#count");
+
+                    counter.innerHTML = "";
+
+                    var countertext = document.createElement("p");
 
 
-                countertext.innerHTML = ("Antal meddelande: " + MessageBoard.messages.length);
+                    countertext.innerHTML = ("Antal meddelande: " + MessageBoard.messages.length);
 
-                counter.appendChild(countertext);
+                    counter.appendChild(countertext);
 
 
+
+                }
+
+                if (result == false) {
+
+                    // ...Klickar användaren på "cancel" händer ingeting alls.
+
+                }
+
+                
             }
         }
         
-        
+        //Vi är fortfarande i rendermessages!! Skapar en datum-knapp, med klassnamn date.
                         
         var datebutton = document.createElement("button");
         
         datebutton.setAttribute("value", "Datum");
-        //datebutton.id = MessageID;
+        
         datebutton.className = "date";
         present.appendChild(datebutton);
+
+            //lägger in alla med klassnamn date i en variabel. Loopar igenom som array. Lägger på en oncklick. En alertruta kommer upp med datum. (datumet formaterat i getDateText() funktionen i Message.js)
 
             var dateelements = document.getElementsByClassName("date");
 
             for (var i = 0; i < dateelements.length; i += 1) {
 
                 dateelements[i].onclick = function () {
-                    console.log(MessageBoard.messages[MessageID]);
+                   
                     var date = MessageBoard.messages[MessageID].getDateText();
 
                     alert(date);
-
-
+                    
+                    
                 }
+
 
             }
 
