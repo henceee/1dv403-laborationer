918a942ffd730096faf09c6b44d13769e3947b22
henceee <hg222dv@student.lnu.se>
2013-12-14 22:40:28 +0100

    memory 1-8 .. det ar ngt iaf

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,14 +1,59 @@
 "use strict";
 
+var count = 0;
+
 var Memory = {
 
     random: [],
 
     init: function () {
 
-        alert("hej");
-    }
+        var randomgen = RandomGenerator.getPictureArray(4, 4);
 
+        Memory.random.push(randomgen);
+
+        console.log(Memory.random);
+        
+        var picplace = document.getElementsByClassName("bild");
+       
+        console.log(picplace);
+
+        
+        var linkpicstring = "<a href='#'><img src='pics/0.png' /></a>";
+
+        for (var i = 0; i < picplace.length; i += 1) {
+
+            picplace[i].innerHTML = linkpicstring;
+        }
+
+
+    },
+
+   
 };
 
 window.onload = Memory.init;
+
+
+
+
+
+
+
+
+
+
+
+
+//var a = document.createElement("a");
+
+//a.innerHTML 
+
+//a.setAttribute("href", "#");
+
+//var pic = document.createElement("img");
+
+//pic.setAttribute("src", "../pics/0.png");
+
+
+//picplace[i].appendChild(a);
